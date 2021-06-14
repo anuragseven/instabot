477ab5fd4faf62472569f31833431490e0ef03f2
anuragseven <anuragtseven@gmail.com>
2021-06-14 19:28:57 +0530

    final commit

--- credentials.py
+++ credentials.py
@@ -1,8 +1,4 @@
-user_access_token = "EAAGqRhZC50kwBAFptgQCdOq3eHceCtHc16QjJ5somyfOOys4okmPdb9ZCwW20nVYONTgfKQORMaAnkshjc1oheAxAXZAdEJ" \
-                    "H7mZCS83jUSXYHtwfZBJvCUZBnRgPOtE0WoJTvQ2tXXbtGzZAUFdb89NG0ZBzUcfZBCaPZCotPfHtnPNNqYmYO5iZBAb"
-instagram_user_id = '17841446044529868'
-azure_cosmos_db_uri = 'mongodb://datahunkbot' \
-                      ':UYiwf4TFxPW2GH4Abv9RLYMSzFP6k8RBPPF4pC6JI0LzjiR8oRYD8GczVocq9W2fd8qquyczEA6id2wLGnsTkw' \
-                      '==@datahunkbot.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false' \
-                      '&maxIdleTimeMS=120000&appName=@datahunkbot@ '
-azure_cosmos_db_key = 'UYiwf4TFxPW2GH4Abv9RLYMSzFP6k8RBPPF4pC6JI0LzjiR8oRYD8GczVocq9W2fd8qquyczEA6id2wLGnsTkw=='
+user_access_token = ""
+instagram_user_id = ""
+azure_cosmos_db_uri = ""
+azure_cosmos_db_key = ""
